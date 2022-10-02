8613148b8afc936c0fc788e2afc8dbcf29e6b2a5
Arthur mendes <arthurmcunha123@gmail.com>
2022-10-01 22:16:52 -0300

    etapas done

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '55123',
+                nome: 'José',
+                partido: 'ABC',
+                fotos: [
+                    {url: '55123.jpg', legenda: 'Deputado estadual'}
+                ]
+            },
+            {
+                numero: '12345',
+                nome: 'Antonella',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '12345.jpg', legenda: 'Deputado estadual'}
+                ]
+            },
+            {
+                numero: '87654',
+                nome: 'Rita',
+                partido: 'ABC',
+                fotos: [
+                    {url: '87654.jpg', legenda: 'Deputado estadual'}
+                ]
+            },
+            {
+                numero: '92001',
+                nome: 'Fábio',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '92001.jpg', legenda: 'Deputado estadual'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '68',
+                nome: 'Lívia',
+                partido: 'ABC',
+                vice: 'Sebastião',
+                fotos: [
+                    {url: '68.jpg', legenda: 'Governador'},
+                    {url: '68_2.jpg', legenda: 'Vice-Governador', small: true}
+                ] 
+            },
+            {
+                numero: '42',
+                nome: 'Manuel',
+                partido: 'DEFG',
+                vice: 'Martin',
+                fotos: [
+                    {url: '42.jpg', legenda: 'Governador'},
+                    {url: '42_2.jpg', legenda: 'Vice-Governador', small: true}
+                ] 
+            }
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,3 @@
+let seuVoto = document.querySelector('.d-1-1 span')
+let cargo = document.querySelector('.d-1-2')
+let descricao = document.querySelector('.d-1-4')
